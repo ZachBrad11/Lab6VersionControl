3935a91e58731021fc1c987ccbc8156d50daf1f1
ZachBrad11 <zach.bradley1218.2002@gmail.com>
2023-07-19 18:27:37 -0400

    Added comments to file

--- VersionControl.py
+++ VersionControl.py
@@ -1,30 +1,39 @@
-def encode(password):
-    output_password = ""
+def encode(password):  # encodes a password by adding 3 to each number
+    output_password = ""  # output will be a string
     for number in password:
-        temp_number = int(number) + 3
-        if temp_number >= 10:
+        temp_number = int(number) + 3  # adds 3 to number
+        if temp_number >= 10:  # if number is greater than 10, subtracts 10
             temp_number -= 10
-        output_password += str(temp_number)
+        output_password += str(temp_number)  # string grows each iteration
     return output_password
         
         
 def main():
     while True:
+
+        # prints the menu
+
         print("Menu\n\n"
               "-------------\n\n"
               "1. Encode\n\n"
               "2. Decode\n\n"
               "3. Quit\n\n\n")
+
+        # user input for menu
+
         option = int(input("Please enter an option: "))
-        if option == 1:
+
+        # Goes through options and follows path chosen by user
+
+        if option == 1:  # encodes password
             password = input("Please enter your password to encode: ")
             password = encode(password)
             print("Your password has been encoded and stored!\n")
-        elif option == 2:
+        elif option == 2:  # left blank for partner to modify
             pass
-        elif option == 3:
+        elif option == 3:  # ends function
             break
-        else:
+        else:  # tells user to enter a valid menu number
             print("Error! Enter a valid input")
             
             
